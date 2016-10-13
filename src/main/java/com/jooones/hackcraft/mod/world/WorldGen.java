31a090b6a8e3eb9a71f0034293a40f66cda50db7
Briers Tom <tom.briers@staff.telenet.be>
2016-10-13 21:10:38 +0200

    world gen

--- src/main/java/com/jooones/hackcraft/mod/world/WorldGen.java
+++ src/main/java/com/jooones/hackcraft/mod/world/WorldGen.java
@@ -8,12 +8,19 @@ import net.minecraft.world.World;
 import net.minecraft.world.chunk.IChunkGenerator;
 import net.minecraft.world.chunk.IChunkProvider;
 import net.minecraft.world.gen.feature.WorldGenMinable;
+import net.minecraft.world.gen.feature.WorldGenerator;
 import net.minecraftforge.fml.common.IWorldGenerator;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
 import java.util.Random;
 
 public class WorldGen implements IWorldGenerator {
+
+    private WorldGenerator warpiumOreGenerator;
+
+    public WorldGen() {
+        this.warpiumOreGenerator = new WorldGenMinable(WarpiumOre.warpiumOre().getDefaultState(), 8);
+    }
 
     @Initialize
     public static void init() {
@@ -24,7 +31,8 @@ public class WorldGen implements IWorldGenerator {
     public void generate(Random random, int chunkX, int chunkZ, World world, IChunkGenerator iChunkGenerator, IChunkProvider iChunkProvider) {
         switch (world.provider.getDimension()) {
             case 0: //Overworld
-                generateOre(world, random, chunkX, chunkZ, 128, 35, 10, WarpiumOre.warpiumOre().getDefaultState());
+                //generateOre(world, random, chunkX, chunkZ, 128, 35, 10, WarpiumOre.warpiumOre().getDefaultState());
+                runGenerator(warpiumOreGenerator, world, random, chunkX, chunkZ, 35, 0, 128);
                 break;
             case -1: //Nether
                 break;
@@ -42,6 +50,19 @@ public class WorldGen implements IWorldGenerator {
             int y = rand.nextInt(maxY);
             BlockPos blockPos = new BlockPos(firstBlockXCoord, y, firstBlockZCoord);
             (new WorldGenMinable(blockState, veinSize)).generate(world, rand, blockPos);
+        }
+    }
+
+    private void runGenerator(WorldGenerator generator, World world, Random rand, int chunk_X, int chunk_Z, int chancesToSpawn, int minHeight, int maxHeight) {
+        if (minHeight < 0 || maxHeight > 256 || minHeight > maxHeight)
+            throw new IllegalArgumentException("Illegal Height Arguments for WorldGenerator");
+
+        int heightDiff = maxHeight - minHeight + 1;
+        for (int i = 0; i < chancesToSpawn; i ++) {
+            int x = chunk_X * 16 + rand.nextInt(16);
+            int y = minHeight + rand.nextInt(heightDiff);
+            int z = chunk_Z * 16 + rand.nextInt(16);
+            generator.generate(world, rand, new BlockPos(x, y, z));
         }
     }
 }
