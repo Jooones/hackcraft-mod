ad5860ddfa43c2ae657daf24eb3f918a4dea5fd4
Jooones <Jooones@users.noreply.github.com>
2016-10-14 01:55:11 +0200

    Jooones: Added necessary portal methods (now that is what I call copy-pasting!); added right-click trigger to spawn portal; added animation to WarpiumBlock placement

--- src/main/java/com/jooones/hackcraft/mod/block/WarpiumBlock.java
+++ src/main/java/com/jooones/hackcraft/mod/block/WarpiumBlock.java
@@ -2,7 +2,19 @@ package com.jooones.hackcraft.mod.block;
 
 import com.jooones.hackcraft.mod.annotation.Initialize;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.effect.EntityLightningBolt;
+import net.minecraft.entity.item.EntityItem;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+import javax.annotation.Nullable;
 
 public class WarpiumBlock extends BaseBlock {
 
@@ -20,14 +32,48 @@ public class WarpiumBlock extends BaseBlock {
         setCreativeTab(CreativeTabs.BUILDING_BLOCKS);
     }
 
-
     public static WarpiumBlock warpiumBlock() {
         if(instance == null) {
             instance = new WarpiumBlock();
         }
-
         return instance;
     }
 
+    @Override
+    public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn, EnumHand hand, @Nullable ItemStack heldItem, EnumFacing side, float hitX, float hitY, float hitZ) {
+        WarpiumPortal.warpiumPortal().trySpawnPortal(worldIn, calculatePositionToEvaluatePortal(pos, side));
+        return super.onBlockActivated(worldIn, pos, state, playerIn, hand, heldItem, side, hitX, hitY, hitZ);
+    }
+
+    private BlockPos calculatePositionToEvaluatePortal(BlockPos pos, EnumFacing side) {
+        BlockPos positionToEvaluatePortal = new BlockPos(pos.getX(), pos.getY(), pos.getZ());
+        if (side == EnumFacing.UP) {
+            positionToEvaluatePortal = new BlockPos(pos.getX(), pos.getY() + 1, pos.getZ());
+        }
+        if (side == EnumFacing.DOWN) {
+            positionToEvaluatePortal = new BlockPos(pos.getX(), pos.getY() - 1, pos.getZ());
+        }
+        if (side == EnumFacing.EAST) {
+            positionToEvaluatePortal = new BlockPos(pos.getX() + 1, pos.getY(), pos.getZ());
+        }
+        if (side == EnumFacing.WEST) {
+            positionToEvaluatePortal = new BlockPos(pos.getX() - 1, pos.getY(), pos.getZ());
+        }
+        if (side == EnumFacing.NORTH) {
+            positionToEvaluatePortal = new BlockPos(pos.getX(), pos.getY(), pos.getZ() - 1);
+        }
+        if (side == EnumFacing.SOUTH) {
+            positionToEvaluatePortal = new BlockPos(pos.getX(), pos.getY(), pos.getZ() + 1);
+        }
+        return positionToEvaluatePortal;
+    }
+
+    @Override
+    public void onBlockAdded(World worldIn, BlockPos pos, IBlockState state) {
+        super.onBlockAdded(worldIn, pos, state);
+        EntityLightningBolt entityLightningBolt = new EntityLightningBolt(worldIn, pos.getX(), pos.getY(), pos.getZ(), true);
+        worldIn.addWeatherEffect(entityLightningBolt);
+//        worldIn.spawnEntityInWorld(new EntityItem(worldIn, pos.getX(), pos.getY(), pos.getZ(), new ItemStack(Items.APPLE)));
+    }
 
 }
